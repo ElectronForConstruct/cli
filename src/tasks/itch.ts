99ec74a1ede7e780bfae571d61e7f9ac342cc017
Quentin Goinaud <armaldio@gmail.com>
2020-07-08 14:40:18 +0200

    -

--- src/tasks/itch.ts
+++ src/tasks/itch.ts
@@ -6,7 +6,7 @@ import { exec } from 'child_process';
 import signale from 'signale';
 import os from 'os';
 import { createScopedLogger } from '../utils/console';
-import Task from '../classes/Task';
+import Task from '../classes/task';
 
 interface Config {
   project: string | null
@@ -45,9 +45,9 @@ interface ItchIoLogger {
 
 const exe = async (
   command: string,
-  logger: signale.Signale<signale.DefaultMethods>,
-): Promise<string> => {
-  logger.log('Publishing to itch...');
+): Promise<{ code: string; log: string[] }> => {
+  const log: string[] = [];
+  console.log('command', command);
   return new Promise((resolve) => {
     const npmstart = exec(command);
 
@@ -57,9 +57,9 @@ const exe = async (
           const json: ItchIoLogger = JSON.parse(data.toString()) as ItchIoLogger;
 
           if (json.type === 'log') {
-            logger.log(json.message);
+            log.push(json.message);
           } else if (json.type === 'progress') {
-            logger.log(`Uploading ${Math.round(json.progress * 100)}% - ${json.eta}s`);
+            log.push(`Uploading ${Math.round(json.progress * 100)}% - ${json.eta}s`);
           }
           // logger.log(data.toString());
         } catch (e) {
@@ -68,11 +68,15 @@ const exe = async (
       });
 
       npmstart.stderr.on('data', (data: Buffer) => {
-        logger.error(data.toString());
+        log.push(data.toString());
       });
 
       npmstart.on('exit', (code) => {
-        resolve((code ?? '').toString());
+        console.log('code', code);
+        resolve({
+          code: (code ?? '').toString(),
+          log,
+        });
       });
     }
   });
@@ -88,45 +92,55 @@ export default {
     });
     const fixed = createScopedLogger('itch');
 
-    const itchFolderPath = path.join(process.cwd(), 'itch');
-    const butler = path.join(itchFolderPath, `butler${os.platform() === 'win32' ? '.exe' : ''}`);
+    // const itchFolderPath = path.join(process.cwd(), 'itch');
+    // const butler = path.join(itchFolderPath, `butler${os.platform() === 'win32' ? '.exe' : ''}`);
 
     // download butler
-    if (!fs.existsSync(itchFolderPath) || !fs.existsSync(butler)) {
-      const platform = os.platform();
-      const arch = os.arch();
+    // if (!fs.existsSync(itchFolderPath) || !fs.existsSync(butler)) {
+    //   const platform = os.platform();
+    //   const arch = os.arch();
 
-      const links = [
-        // get         remote
-        ['darwin-x64', 'darwin-amd64'],
-        ['linux-x64', 'linux-amd64'],
-        ['linux-ia32', 'linux-386'],
-        ['win32-x64', 'windows-amd64'],
-        ['win32-ia32', 'windows-386'],
-      ];
+    //   const links = [
+    //     // get         remote
+    //     ['darwin-x64', 'darwin-amd64'],
+    //     ['linux-x64', 'linux-amd64'],
+    //     ['linux-ia32', 'linux-386'],
+    //     ['win32-x64', 'windows-amd64'],
+    //     ['win32-ia32', 'windows-386'],
+    //   ];
 
-      const foundLink = links.find((link) => link[0] === `${platform}-${arch}`);
-      if (!foundLink) {
-        return {
-          error: true,
-          sources: [],
-        };
-      }
-      const butlerPlatform = foundLink[1];
-      const linkToDownload = `https://broth.itch.ovh/butler/${butlerPlatform}/LATEST/archive/default`;
+    //   const foundLink = links.find((link) => link[0] === `${platform}-${arch}`);
+    //   if (!foundLink) {
+    //     return {
+    //       error: true,
+    //       sources: [],
+    //     };
+    //   }
+    //   const butlerPlatform = foundLink[1];
+    //   const linkToDownload = `https://broth.itch.ovh/butler/${butlerPlatform}/LATEST/archive/default`;
 
-      await fs.ensureDir(itchFolderPath);
+    //   await fs.ensureDir(itchFolderPath);
 
-      const sourceZip = await downloadFile(linkToDownload, path.join(itchFolderPath, 'itch.zip'));
-      await extractZip(sourceZip, itchFolderPath);
+    //   const sourceZip =
+    // await downloadFile(linkToDownload, path.join(itchFolderPath, 'itch.zip'));
+    //   await extractZip(sourceZip, itchFolderPath);
 
-      await fs.remove(sourceZip);
+    //   await fs.remove(sourceZip);
+    // }
+
+    // await fs.chmod(butler, '755');
+
+    // await exe(`${butler} upgrade -j`, logger);
+    // await exe(`${butler} login`, logger);
+    const butler = 'butler';
+
+    const result = await exe(`${butler} -V`);
+    console.log('result', result);
+    if (result[0] !== 'v') {
+      logger.error('Butler not found');
     }
 
-    await fs.chmod(butler, '755');
-
-    await exe(`${butler} upgrade -j`, logger);
-    await exe(`${butler} login`, logger);
+    // console.log('workingDirectory', workingDirectory);
 
     const { project, directories } = taskSettings as Config;
     if (!project) {
@@ -137,6 +151,8 @@ export default {
       };
     }
 
+    // console.log('directories', directories);
+
     let uploaded = false;
     for (let i = 0; i < directories.length; i += 1) {
       const directory = directories[i];
@@ -144,14 +160,15 @@ export default {
       const outDir = path.resolve(process.cwd(), directory.name);
 
       if (outDir === workingDirectory) {
-        await exe(`${butler} push ${outDir} ${project}:${directory.channel} -j`, logger);
+        logger.log('Publishing to itch...');
+        // await exe(`${butler} push ${outDir} ${project}:${directory.channel} -j`, logger);
         logger.success(`${directory.channel} uploaded successfully`);
         uploaded = true;
       }
     }
 
     if (!uploaded) {
-      logger.warn(`Match for "${workingDirectory}" not found. Skipping.`);
+      logger.warn(`No match found in configuration for "${workingDirectory}". Skipping.`);
     }
 
     return {
