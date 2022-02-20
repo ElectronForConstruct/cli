65c484033b798e0ece845bec3504b57c18bf751e
Armaldio <armaldio@gmail.com>
2022-02-20 11:43:09 +0100

    -

--- packages/front/src/main.ts
+++ packages/front/src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import { BaklavaVuePlugin } from '@baklavajs/plugin-renderer-vue3';
+
+import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
 import '@baklavajs/plugin-renderer-vue3/dist/styles.css';
 
-
 const app = createApp(App);
 
 app.use(BaklavaVuePlugin);
-app.use(store)
-app.use(router)
+app.use(store);
+app.use(router);
 app.mount('#app');
